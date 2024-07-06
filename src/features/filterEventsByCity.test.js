e109f5a033111f455cc1c63bdc8eb1941272a234
capmec <capmec@gmail.com>
2024-07-06 17:15:31 +0200

    scenario 2 done

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,6 +1,7 @@
 import { loadFeature, defineFeature } from 'jest-cucumber'
 import { render, within, waitFor } from '@testing-library/react'
 import App from '../App'
+import userEvent from '@testing-library/user-event'
 import { getEvents } from '../mock-data'
 
 const feature = loadFeature('./src/Features/filterEventsByCity.feature')
@@ -33,13 +34,29 @@ defineFeature(feature, (test) => {
     when,
     then,
   }) => {
-    given('the main page is open', () => {})
+    let AppComponent
+    given('the main page is open', () => {
+      AppComponent = render(<App />)
+    })
 
-    when('the user starts typing in the city textbox', () => {})
+    let CitySearchDOM
+    when('the user starts typing in the city textbox', async () => {
+      const user = userEvent.setup()
+      const AppDOM = AppComponent.container.firstChild
+      CitySearchDOM = AppDOM.querySelector('#city-search')
+      const citySearchInput = within(CitySearchDOM).queryByRole('textbox')
+      await user.type(citySearchInput, 'Berlin')
+    })
 
     then(
       'the user should see a list of cities (suggestions) that match what they’ve typed',
-      () => {}
+      async () => {
+        await waitFor(() => {
+          const suggestionListItems =
+            within(CitySearchDOM).queryAllByRole('listitem')
+          expect(suggestionListItems).toHaveLength(2)
+        })
+      }
     )
   })
 
