c99080ac239ab6f6c1ca56666c4f4fda605679fe
capmec <capmec@gmail.com>
2024-07-06 18:31:20 +0200

    next: start task

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -2,7 +2,7 @@ import { loadFeature, defineFeature } from 'jest-cucumber'
 import { render, within, waitFor } from '@testing-library/react'
 import App from '../App'
 import userEvent from '@testing-library/user-event'
-import { getEvents } from '../mock-data'
+import { getEvents } from '../api'
 
 const feature = loadFeature('./src/Features/filterEventsByCity.feature')
 
@@ -66,23 +66,52 @@ defineFeature(feature, (test) => {
     when,
     then,
   }) => {
-    given('the user was typing “Berlin” in the city textbox', () => {})
+    let AppComponent
+    let AppDOM
+    let CitySearchDOM
+    let citySearchInput
 
-    and('the list of suggested cities is showing', () => {})
+    given('the user was typing “Berlin” in the city textbox', async () => {
+      AppComponent = render(<App />)
+      const user = userEvent.setup()
+      AppDOM = AppComponent.container.firstChild
+      CitySearchDOM = AppDOM.querySelector('#city-search')
+      citySearchInput = within(CitySearchDOM).queryByRole('textbox')
+      await user.type(citySearchInput, 'Berlin')
+    })
+
+    let suggestionListItems
+    and('the list of suggested cities is showing', () => {
+      suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem')
+      expect(suggestionListItems).toHaveLength(2)
+    })
 
     when(
       'the user selects a city (e.g., “Berlin, Germany”) from the list',
-      () => {}
+      async () => {
+        const user = userEvent.setup()
+        await user.click(suggestionListItems[0])
+      }
     )
 
     then(
       'their city should be changed to that city (i.e., “Berlin, Germany”)',
-      () => {}
+      () => {
+        expect(citySearchInput.value).toBe('Berlin, Germany')
+      }
     )
 
     and(
       'the user should receive a list of upcoming events in that city',
-      () => {}
+      async () => {
+        const EventListDOM = AppDOM.querySelector('#event-list')
+        const EventListItems = within(EventListDOM).queryAllByRole('listitem')
+        const allEvents = await getEvents()
+        const eventsInBerlin = allEvents.filter(
+          (event) => event.location === 'Berlin, Germany'
+        )
+        expect(EventListItems.length).toBe(eventsInBerlin.length)
+      }
     )
   })
 })
