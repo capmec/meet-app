95ae06becb7394c2ba7397d2e45e968e3c8f4601
capmec <capmec@gmail.com>
2024-07-08 18:08:29 +0200

    start task

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import EventList from './components/EventList'
 import NumberOfEvents from './components/NumberOfEvents'
 import { useEffect, useState } from 'react'
 import { extractLocations, getEvents } from './api'
+import { InfoAlert } from './components/Alert'
 
 import './App.css'
 
@@ -13,6 +14,8 @@ const App = () => {
   const [numberOfEvents, setNumberOfEvents] = useState(32)
   const [events, setEvents] = useState([])
   const [currentCity, setCurrentCity] = useState('See all cities')
+  const [infoAlert, setInfoAlert] = useState('')
+  const [errorAlert, setErrorAlert] = useState('')
 
   useEffect(() => {
     fetchData()
@@ -30,7 +33,14 @@ const App = () => {
 
   return (
     <div className='App'>
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <div className='alerts-container'>
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+      </div>
+      <CitySearch
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert}
+      />
       <NumberOfEvents setNumberOfEvents={setNumberOfEvents} />
       <EventList events={events} />
     </div>
