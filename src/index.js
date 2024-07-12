061ccdeb67e0969f483eca1b81b1bc206b55a3e2
capmec <capmec@gmail.com>
2024-07-12 18:43:42 +0200

    4.9: Progressive Web Applications

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
 
 import * as atatus from 'atatus-spa'
-atatus.config('45f81883611c4a1c909200f1238b9794').install()
+atatus.config('6a5faafc38ba4221b897216633425e8f').install()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -18,6 +18,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.unregister();
 serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
